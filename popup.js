c617ffa5f4b1f4a4086c57bc6a285a2261aaf076
Peyman Naderi <peymannaderi10>
2023-07-31 20:01:45 -0400

    optimize and reduce

--- popup.js
+++ popup.js
@@ -1,51 +1,66 @@
-const bionicReaderSwitch = document.getElementById("bionicReaderSwitch");
-
-
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  const tabId = tabs[0].id;
-  chrome.storage.sync.get(["tabStates"], ({ tabStates }) => {
-    const isEnabled = tabStates?.[tabId] ?? false;
-    bionicReaderSwitch.checked = isEnabled;
-  });
-});
-
-bionicReaderSwitch.addEventListener("change", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const tabId = tabs[0].id;
-    const isEnabled = bionicReaderSwitch.checked;
-    chrome.storage.sync.get(["tabStates"], ({ tabStates }) => {
-      tabStates = tabStates ?? {};
-      tabStates[tabId] = isEnabled;
-      chrome.storage.sync.set({ tabStates });
-      chrome.tabs.sendMessage(
-        tabId,
-        { bionicReaderEnabled: isEnabled },
-        (response) => {
-          if (response?.result === "success") {
-            console.log("Bionic Reader applied/removed successfully.");
-          } else {
-            console.error("Error applying/removing Bionic Reader.");
-          }
+function modifyDOM(action, boldPercent) {
+    var elements = document.querySelectorAll('p,h1,h2,h3,h4,h5,h6,li');
+    elements.forEach(function(elem) {
+        if (action === 'increase' || action === 'decrease') {
+            var style = window.getComputedStyle(elem, null).getPropertyValue('line-height');
+            var lineHeight = parseFloat(style);
+    
+            if (action === 'increase') {
+                lineHeight *= 1.2;
+            } else if (action === 'decrease') {
+                lineHeight /= 1.2;
+            }
+            elem.style.lineHeight = lineHeight + 'px';
+        } else if (action === 'toggleBold') {
+            var words = elem.innerText.split(' ');
+            var newContent = '';
+            words.forEach(function(word) {
+                var boldCharCount = Math.floor(word.length * boldPercent);
+                newContent += '<b>' + word.substr(0, boldCharCount) + '</b>' + word.substr(boldCharCount) + ' ';
+            });
+            elem.innerHTML = newContent;
+        } else if (action === 'untoggleBold') {
+            elem.innerHTML = elem.innerText;
         }
-      );
     });
-  });
+}
+
+document.getElementById('increase').addEventListener('click', function() {
+    chrome.tabs.executeScript({
+        code: '(' + modifyDOM + ')("increase", 0);'
+    });
 });
 
-const increaseLineHeightButton = document.getElementById("increaseLineHeight");
-const decreaseLineHeightButton = document.getElementById("decreaseLineHeight");
-
-increaseLineHeightButton.addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const message = { action: "increase" };
-    chrome.tabs.sendMessage(tabs[0].id, message);
-  });
+document.getElementById('decrease').addEventListener('click', function() {
+    chrome.tabs.executeScript({
+        code: '(' + modifyDOM + ')("decrease", 0);'
+    });
 });
 
-decreaseLineHeightButton.addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const message = { action: "decrease" };
-    chrome.tabs.sendMessage(tabs[0].id, message);
-  });
+var boldRange = document.getElementById('boldRange');
+var boldPercent = document.getElementById('boldPercent');
+boldRange.addEventListener('input', function() {
+    var percent = (parseInt(this.value) + 1) * 10 + 20;
+    boldPercent.textContent = percent + '%';
+
+    // Check if toggleBold checkbox is checked, and if so, apply changes immediately
+    if (document.getElementById('toggleBold').checked) {
+        var percent = (parseInt(this.value) + 1) / 10 + 0.2;
+        chrome.tabs.executeScript({
+            code: '(' + modifyDOM + ')("toggleBold", ' + percent + ');'
+        });
+    }
 });
 
+document.getElementById('toggleBold').addEventListener('change', function() {
+    if (this.checked) {
+        var percent = (parseInt(boldRange.value) + 1) / 10 + 0.2;
+        chrome.tabs.executeScript({
+            code: '(' + modifyDOM + ')("toggleBold", ' + percent + ');'
+        });
+    } else {
+        chrome.tabs.executeScript({
+            code: '(' + modifyDOM + ')("untoggleBold", 0);'
+        });
+    }
+});
